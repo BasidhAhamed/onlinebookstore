c85db31a4ea0218f5421b0a403a7d155a7f4d4bc
Basidh Ahamed <66265324+BasidhAhamed@users.noreply.github.com>
2022-11-19 15:13:40 +0530

    Update database_functions.php

--- obs/functions/database_functions.php
+++ obs/functions/database_functions.php
@@ -16,6 +16,11 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "basidhahamed", "pwd" => "{your_password_here}", "Database" => "onlinbookstore", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:online-bookstore.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
 
 	function select4LatestBook($conn){
 		$row = array();
